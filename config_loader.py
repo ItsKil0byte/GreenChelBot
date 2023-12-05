0cc6e38a94b9da863fae49bdf18e607c76297d59
Alexander Lebedev <itskilobyte@gmail.com>
2023-12-05 14:58:23 +0500

    Add CI pipelines

--- config_loader.py
+++ config_loader.py
@@ -4,6 +4,6 @@ from pydantic import SecretStr
 class Settings(BaseSettings):
     bot_token: SecretStr
     admin_id: SecretStr
-    model_config: SettingsConfigDict = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config: SettingsConfigDict = SettingsConfigDict(env_file="secrets/.env", env_file_encoding="utf-8")
 
 config = Settings()
